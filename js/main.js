542e21e7d332f6c592e423be71211d3126dd12d7
Mauro De Luca <39085015+maurodeluca@users.noreply.github.com>
2026-01-26 19:24:47 +0000

    Fixes

--- js/main.js
+++ js/main.js
@@ -50,7 +50,9 @@ const manifestoTarget = document.getElementById('manifesto');
 let idx = 0;
 
 function typeManifesto() {
-  if (!manifestoTarget || idx >= manifestoText.length) return;
+  if (!manifestoTarget) return;
+  if (idx >= manifestoText.length) return;
+  
   manifestoTarget.textContent += manifestoText.charAt(idx);
   idx++;
   setTimeout(typeManifesto, 25); // speed in ms per character
