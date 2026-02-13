6aa1f60ba9a263e100c7e5f14d82a0eced301cc3
Mauro De Luca <39085015+maurodeluca@users.noreply.github.com>
2026-02-13 11:33:14 +0000

    fix

--- js/core/typing.js
+++ js/core/typing.js
@@ -3,36 +3,36 @@ let typingSkipped = false;
 // Parse Markdown to HTML
 function markdownToHtml(text) {
   return text
-    .replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')  // **bold**
-    .replace(/\*(.+?)\*/g, '<em>$1</em>')              // *italic*
-    .replace(/_(.+?)_/g, '<em>$1</em>')                // _italic_
+    .replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
+    .replace(/\*(.+?)\*/g, '<em>$1</em>')
+    .replace(/_(.+?)_/g, '<em>$1</em>')
     .replace(/\n/g, '<br>');
 }
 
 function typeText(text, element, speed = 40, callback) {
-  if (typingSkipped) return;
   if (!element) return;
-  
+
+  typingSkipped = false; // reset when starting a new session
+
   const htmlText = markdownToHtml(text);
   const parser = new DOMParser();
   const doc = parser.parseFromString(`<div>${htmlText}</div>`, 'text/html');
   const nodes = Array.from(doc.body.firstChild.childNodes);
-  
+
   let nodeIndex = 0;
   let charIndex = 0;
   element.innerHTML = '';
 
   function type() {
     if (typingSkipped) {
-      element.innerHTML = htmlText;
-      typingSkipped = false;
+      element.innerHTML = htmlText; // show full content
       if (callback) callback();
       return;
     }
-    
+
     if (nodeIndex < nodes.length) {
       const currentNode = nodes[nodeIndex];
-      
+
       if (currentNode.nodeType === 3) {
         if (charIndex < currentNode.length) {
           element.appendChild(document.createTextNode(currentNode.data[charIndex]));
@@ -53,13 +53,12 @@ function typeText(text, element, speed = 40, callback) {
   }
 
   type();
-  typingSkipped = false;
-
 }
 
+// Skip typing but keep HTML formatting
 function skipTyping(target, text) {
-  target.textContent = text;
-  typingSkipped = false;
+  target.innerHTML = markdownToHtml(text);
+  typingSkipped = true;
 }
 
 function setTypingSkipped(value) {
